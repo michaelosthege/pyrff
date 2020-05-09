fb8f22c18b752577eaa3d13c1cde5c61ad541f27
Michael Osthege <michael.osthege@outlook.com>
2020-05-09 14:41:34 +0200

    fix bug in exception implementation

--- pyrff/test_exceptions.py
+++ pyrff/test_exceptions.py
@@ -5,23 +5,37 @@ from . import exceptions
 
 class TestExceptions:
     def test_dtype_error(self):
-        with pytest.raises(exceptions.DtypeError):
+        with pytest.raises(exceptions.DtypeError) as exinfo:
             raise exceptions.DtypeError('Just the message.')
-        with pytest.raises(exceptions.DtypeError):
+        assert 'Just' in exinfo.value.args[0]
+
+        with pytest.raises(exceptions.DtypeError) as exinfo:
             raise exceptions.DtypeError('With types.', actual=str)
-        with pytest.raises(exceptions.DtypeError):
-            raise exceptions.DtypeError('With types.', expected=str)
-        with pytest.raises(exceptions.DtypeError):
+        assert 'str' in exinfo.value.args[0]
+
+        with pytest.raises(exceptions.DtypeError) as exinfo:
+            raise exceptions.DtypeError('With types.', expected=float)
+        assert 'float' in exinfo.value.args[0]
+
+        with pytest.raises(exceptions.DtypeError) as exinfo:
             raise exceptions.DtypeError('With types.', actual=int, expected=str)
+        assert 'int' in exinfo.value.args[0] and 'str' in exinfo.value.args[0]
         pass
 
     def test_shape_error(self):
-        with pytest.raises(exceptions.ShapeError):
+        with pytest.raises(exceptions.ShapeError) as exinfo:
             raise exceptions.ShapeError('Just the message.')
-        with pytest.raises(exceptions.ShapeError):
+        assert 'Just' in exinfo.value.args[0]
+
+        with pytest.raises(exceptions.ShapeError) as exinfo:
             raise exceptions.ShapeError('With shapes.', actual=(2,3))
-        with pytest.raises(exceptions.ShapeError):
-            raise exceptions.ShapeError('With shapes.', expected='(2,3) or (5,6')
-        with pytest.raises(exceptions.ShapeError):
+        assert '(2, 3)' in exinfo.value.args[0]
+
+        with pytest.raises(exceptions.ShapeError) as exinfo:
+            raise exceptions.ShapeError('With shapes.', expected='(2,3) or (5,6)')
+        assert '(5,6)' in exinfo.value.args[0]
+
+        with pytest.raises(exceptions.ShapeError) as exinfo:
             raise exceptions.ShapeError('With shapes.', actual=(), expected='(5,4) or (?,?,6)')
+        assert '(?,?,6)' in exinfo.value.args[0]
         pass
