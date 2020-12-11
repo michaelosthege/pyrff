b9095b842288a7044b0e4f71dccc563f39ad37ef
Michael Osthege <m.osthege@fz-juelich.de>
2020-12-11 18:19:43 +0100

    bump to 2.0.1

--- pyrff/__init__.py
+++ pyrff/__init__.py
@@ -3,4 +3,4 @@ from . rff import sample_rff, save_rffs, load_rffs
 from . thompson import sample_batch, sampling_probabilities
 from . utils import multi_start_fmin
 
-__version__ = '2.0.0'
+__version__ = '2.0.1'
